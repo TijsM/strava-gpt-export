7490200da40b70e74d5923fb33fce6ada2480ec9
Tijs Martens <tijs.martens.tijs@gmail.com>
2025-01-25 10:20:09 +0100

    feat: include lap data in export

--- components/ExportSection.tsx
+++ components/ExportSection.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { useActivitiesLapStore } from "@/stores/activitiesLapStore";
 import { useActivitiesStore } from "@/stores/activitiesStore";
 import { useSelectedActivities } from "@/stores/selectedActivitiesStore";
 import { styled } from "styled-components";
 
 export const ExportSection = () => {
   const { selectedActivities } = useSelectedActivities();
+  const { laps } = useActivitiesLapStore();
   const { activities, loading } = useActivitiesStore();
 
   const onClickExport = () => {
@@ -26,6 +28,19 @@ export const ExportSection = () => {
         average_heartrate: activity.average_heartrate,
         max_heartrate: activity.max_heartrate,
         suffer_score: activity.suffer_score,
+        laps: laps
+          .filter((lap) => lap.activity.id === activity.id)
+          .map((lap) => ({
+            name: lap.name,
+            elapsed_time: lap.elapsed_time,
+            moving_time: lap.moving_time,
+            distance: lap.distance,
+            average_speed: lap.average_speed,
+            max_speed: lap.max_speed,
+            total_elevation_gain: lap.total_elevation_gain,
+            average_heartrate: lap.average_heartrate,
+            max_heartrate: lap.max_heartrate,
+          })),
       }));
 
     navigator.clipboard.writeText(
